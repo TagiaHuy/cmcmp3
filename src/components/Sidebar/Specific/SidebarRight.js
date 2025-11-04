4c6927dd9cee40cc4b23e951afbbe01804e1b2f6
Hoang. Le Trung - CMC Global TTS <lthoang1@cmcglobal.v>
2025-11-04 16:53:16 +0700

    suggestion

--- src/components/Sidebar/Specific/SidebarRight.js
+++ src/components/Sidebar/Specific/SidebarRight.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Box } from '@mui/material';
 import NowPlaying from './NowPlaying';
+import SuggestionList from './SuggestionList';
 
 function SidebarRight() {
   return (
-    <Box sx={{ p: 2, height: '100%', overflowY: 'auto' }}>
+    <Box sx={{ p: 2 }}>
       <NowPlaying />
+      <SuggestionList />
       {/* Các thành phần khác của sidebar phải có thể được thêm vào đây trong tương lai */}
     </Box>
   );
