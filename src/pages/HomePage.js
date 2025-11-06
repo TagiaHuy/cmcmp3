e302a20d3c95c6cb24c790fd13e0a5676e47d83f
TagiaHuy <tagiahuy2605@gmail.com>
2025-11-06 21:39:04 +0700

    v3

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -11,6 +11,7 @@ import song1 from '../assets/slaygirl.jpg';
 import banner from '../assets/anh-ech-meme.jpg';  
 import sampleMusic from '../assets/Yas.mp3'; // Assuming sample.mp3 is in assets
 import BXHNewReleaseSection from '../components/Card/BXHNewReleaseSection';
+import Amthambenem from '../assets/Am-tham-ben-em.mp3';
 
 const sampleBanners = [
   {
