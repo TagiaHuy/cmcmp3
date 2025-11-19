4c60dd26642b4bb2c0c880b5580d66274c9ba954
Ta Gia Huy <tagiahuy2605@gmail.com>
2025-11-19 13:40:50 +0700

    v12

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -2,14 +2,23 @@ import React from 'react';
 import { Box, IconButton } from '@mui/material';
 import ArrowBackOutlinedIcon from '@mui/icons-material/ArrowBackOutlined';
 import ArrowForwardOutlinedIcon from '@mui/icons-material/ArrowForwardOutlined';
+import { useNavigate } from 'react-router-dom';
 
 function Navigation() {
+  const navigate = useNavigate();
+
   return (
     <Box sx={{ backgroundColor: (theme) => theme.navigation.backgroundColor }}>
-      <IconButton sx={{ color: (theme) => theme.navigation.iconColor }}>
+      <IconButton
+        sx={{ color: (theme) => theme.navigation.iconColor }}
+        onClick={() => navigate(-1)}
+      >
         <ArrowBackOutlinedIcon />
       </IconButton>
-      <IconButton sx={{ color: (theme) => theme.navigation.iconColor }}>
+      <IconButton
+        sx={{ color: (theme) => theme.navigation.iconColor }}
+        onClick={() => navigate(1)}
+      >
         <ArrowForwardOutlinedIcon />
       </IconButton>
     </Box>
