9d70361e6c90454a4599aebace447c41ba62ecef
Ta Gia Huy <tagiahuy2605@gmail.com>
2025-11-28 16:07:47 +0700

    v5

--- src/components/MediaPlayer/MediaPlayer.js
+++ src/components/MediaPlayer/MediaPlayer.js
@@ -143,6 +143,12 @@ const MediaPlayer = () => {
     setSelectedLyric(null);
   };
 
+  const handleLyricsParsed = (newLyrics) => {
+    if (!currentTrack) return;
+    updateSongInQueue(currentTrack.id, { lyrics: newLyrics });
+    setSelectedLyric(null);
+  };
+
   useEffect(() => {
     if (playerRef.current) {
       setMediaPlayerHeight(playerRef.current.clientHeight);
@@ -287,6 +293,7 @@ const MediaPlayer = () => {
               onCardTextUpdate={handleCardTextUpdate}
               onCardAdd={handleCardAdd}
               onCardDelete={handleCardDelete}
+              onLyricsParsed={handleLyricsParsed}
           />
         )}
         <Box
