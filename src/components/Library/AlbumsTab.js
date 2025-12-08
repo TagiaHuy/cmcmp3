26610ec2a88a6524976cf3d86c6e3fc2629a0138
nkminh14 <ngyenkhoaminh0104@gmail.com>
2025-12-08 14:01:52 +0700

    trang album

--- src/components/Library/AlbumsTab.js
+++ src/components/Library/AlbumsTab.js
@@ -1,11 +1,30 @@
-import React from 'react';
-import { Typography } from '@mui/material';
+import React, { useState } from 'react';
+import { Box, Button, ButtonGroup } from '@mui/material';
+import FavoriteAlbums from '../Album/FavoriteAlbums';
+import UserAlbums from '../Album/UserAlbums';
 
 const AlbumsTab = () => {
+  const [view, setView] = useState('favorites'); // 'favorites' or 'user_created'
+
   return (
-    <Typography>
-      Nội dung các album sẽ được hiển thị ở đây.
-    </Typography>
+    <Box sx={{ width: '100%' }}>
+      <ButtonGroup size="small" sx={{ mb: 2 }}>
+        <Button
+          variant={view === 'favorites' ? 'contained' : 'outlined'}
+          onClick={() => setView('favorites')}
+        >
+          Yêu thích
+        </Button>
+        <Button
+          variant={view === 'user_created' ? 'contained' : 'outlined'}
+          onClick={() => setView('user_created')}
+        >
+          Đã tạo
+        </Button>
+      </ButtonGroup>
+
+      {view === 'favorites' ? <FavoriteAlbums /> : <UserAlbums />}
+    </Box>
   );
 };
 
